ac69e4ba756a50241d4af8ccca212a87ff0b3eae
Dusan Terzic <dterzic@gmail.com>
2019-07-05 10:32:48 +0200

    Initial commit

---
+++ Example/CalendarWrapper/GCWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GCWAppDelegate.h
+//  CalendarWrapper
+//
+//  Created by Dusan Terzic on 07/05/2019.
+//  Copyright (c) 2019 Dusan Terzic. All rights reserved.
+//
+
+@import UIKit;
+
+@interface GCWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/CalendarWrapper/GCWViewController.m
@@ -0,0 +1,29 @@
+//
+//  GCWViewController.m
+//  CalendarWrapper
+//
+//  Created by Dusan Terzic on 07/05/2019.
+//  Copyright (c) 2019 Dusan Terzic. All rights reserved.
+//
+
+#import "GCWViewController.h"
+
+@interface GCWViewController ()
+
+@end
+
+@implementation GCWViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
