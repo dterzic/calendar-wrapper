ac35b4fd8b13361a12c4ea10f6fca3401053c8b0
Dusan Terzic <dusan@envoy.com>
2020-09-29 10:00:03 +0200

    - Fixed bug in `NSString+GCWSmartIcon`.

--- CalendarWrapper/Classes/NSString+GCWSmartIcon.h
+++ CalendarWrapper/Classes/NSString+GCWSmartIcon.h
@@ -1,36 +1,8 @@
-#import "NSString+GCWSmartIcon.h"
+#import <Foundation/Foundation.h>
 
-@implementation NSString (GCWSmartIcon)
+@interface NSString (GCWSmartIcon)
 
-- (NSDictionary *)imageVocabularyDictionary {
-    return @{@"Dinning" : @[@"dinner", @"dinning", @"lunch"],
-             @"Cart" : @[@"shopping", @"store"],
-             @"Drink" : @[@"happy hour", @"drink", @"wine"],
-             @"Moive" : @[@"movie", @"theater"],
-             @"Cake" : @[@"cake", @"birthday"],
-             @"Coffee" : @[@"coffee", @"cafe", @"tea"],
-             @"Fitness" : @[@"fitness", @"workout", @"gym"],
-             @"Planning" : @[@"planning", @"plan", @"calendar"],
-             @"Home" : @[@"home", @"house", @"family"],
-             @"Pen" : @[@"write", @"writing", @"learn", @"learning"],
-             @"Call" : @[@"call", @"phone", @"cell"],
-             @"Moon" : @[@"moon", @"night", @"evening"],
-             @"Keyboard" : @[@"coding", @"code", @"programming"],
-             @"Meeting" : @[@"meeting", @"mtg", @"standup"],
-             @"Sleep" : @[@"sleep", @"rest"]};
-}
-
-- (NSString *)matchIconName {
-    NSString *iconName;
-    for (NSString *key in [self imageVocabularyDictionary].allKeys) {
-        for (NSString *vocabulary in [self imageVocabularyDictionary][key]) {
-            if ([self.lowercaseString containsString:vocabulary]) {
-                iconName = key;
-            }
-        }
-    }
-
-    return iconName;
-}
+- (NSString *)matchIconName;
 
 @end
+
