e24f46d1d6c926e62cee440a7c7895f682cd9502
Dusan Terzic <dusan@envoy.com>
2021-05-27 23:07:01 +0200

    - minor.

--- CalendarWrapper/Classes/GCWCalendar.h
+++ CalendarWrapper/Classes/GCWCalendar.h
@@ -58,7 +58,7 @@
                    success:(void (^_Nullable)(NSDictionary *_Nonnull, NSArray *_Nonnull))success
                    failure:(void (^_Nullable)(NSError *_Nonnull))failure;
 
-- (NSArray *)getFetchedEventsBefore:(NSDate *_Nonnull)startDate andAfter:(NSDate *_Nonnull)endDate;
+- (NSArray *_Nonnull)getFetchedEventsBefore:(NSDate *_Nonnull)startDate andAfter:(NSDate *_Nonnull)endDate;
 
 - (void)syncEventsFrom:(NSDate *_Nonnull)startDate
                     to:(NSDate *_Nonnull)endDate
